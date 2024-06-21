dc9211ccf1b0b7f5b1a1232cf060ea97f41b653c
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-06-21 13:18:05 +0530

    Time: 4 ms (11.75%), Space: 7.2 MB (14.29%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -9,11 +9,15 @@ public:
             for(int j = i-1 ; j>=1 ; j--){
                 if(i%j==0 ){
                     if(dp[i-j]==0)
-                    dp[i]=1;
+                    {dp[i]=1;break;}
                     else
                     dp[i]=0;
                 }
             }
+        }
+
+        for(int i = 0 ; i< n ; i++){
+            cout<<i<<"--"<<dp[i]<<endl;
         }
         return dp[n];
     }
