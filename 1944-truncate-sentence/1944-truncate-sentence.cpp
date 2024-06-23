c82cda5600ec86832cd8611cc6da909ef9b69eda
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-06-23 13:25:37 +0530

    Time: 3 ms (44.83%), Space: 7.6 MB (66.71%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int n = s.size()    ;
+        int count = 0 ;
+
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]==' ')
+                count++;
+            
+            if(count==k)
+                return s.substr(0,i);
+        }
+
+        return s ;
+    }
+};
