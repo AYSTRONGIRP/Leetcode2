c97052cfe91ba17ccd067b102483775b3e54b1a1
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-06-26 17:07:46 +0530

    Time: 0 ms (100%), Space: 12.5 MB (59.96%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int l = 1 ; 
+        int r = n-2 ;
+        if(nums[0]<nums[n-1] || n == 1)
+            return nums[0];
+        if(n==2)
+            return min(nums[0],nums[1]);
+        
+        if(nums[0]<nums[n-1] && nums[0]<nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]<nums[0] && nums[n-1]<nums[n-2]){
+            return nums[n-1];
+        }
+        
+        while(l<=r){
+            long mid = l+(r-l)/2;
+            cout<<mid<<"mid"<<endl;
+            cout<<l<<"l h"<<r<<endl;
+            if(nums[mid]<nums[mid-1] &&nums[mid]<nums[mid+1]){
+                return nums[mid];
+            }
+            // else if(nums[mid]<nums[mid-1] &&nums[mid]<nums[mid+1]){
+            //     return nums[mid];
+            // }
+            
+            else if(nums[mid]<nums[n-1])
+                {
+                    r=mid-1;
+                    // cout<<"
+                }
+            else{
+                    l=mid+1;
+                }
+
+
+        }
+        if(l==n-1){
+            cout<<"last index case"<<l<<r<<endl;
+            return nums[0];}
+        else{
+            cout<<l<<"found in "<<endl;
+            return nums[l];
+        }
+    }
+};
