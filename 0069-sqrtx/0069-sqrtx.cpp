ba4ce593a192492f80634b8044c585f26c63ac54
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-06-26 13:28:02 +0530

    Time: 23 ms (5.49%), Space: 7.4 MB (80.77%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0 ;
+        int r = x ;
+        int count = 1000;
+
+        if(x==1)
+            return 1;
+        if(x==0)
+            return 0 ;
+
+        while(l<r){
+            long long mid = l+(r-l)/2;
+            cout<<mid<<"mid"<<endl;
+            if(mid*mid>x)
+                r=mid;
+            else
+                l=mid+1;
+            
+            count--;
+            if(count==0)
+                return l ;
+        }
+        return l-1 ;
+    }
+};
