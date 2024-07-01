84371a1d4751cc44c1d38226493e84aa43c705ea
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-07-01 17:18:08 +0530

    Time: 0 ms (100%), Space: 10.6 MB (8.09%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int count = 0 ;
-        for(auto it:arr){
+        for(auto &it:arr){
             if(it&1){
                 count++;
                 if(count==3)
