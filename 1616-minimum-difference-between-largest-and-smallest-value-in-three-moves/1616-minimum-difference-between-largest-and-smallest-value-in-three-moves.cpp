78db74e20e608ffeaba45e8b3baa964ab72624d8
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-07-03 11:56:23 +0530

    Time: 71 ms (45.15%), Space: 38.7 MB (80.72%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -7,19 +7,14 @@ public:
             return 0 ;
         
         int i = 0 ; int j = nums.size()-1;
-        // int ix[4] = {0,1,2,3};
-        // int jx[4] =  {3,2,1,0};
-        // int mini = INT_MAX;
-
-        int n = nums.size();
+        int ix[4] = {0,1,2,3};
+        int jx[4] =  {3,2,1,0};
         int mini = INT_MAX;
 
-        // Check the minimum difference by removing different sets of 3 elements
-        mini = std::min(mini, nums[n - 1] - nums[3]); // Remove the 3 smallest
-        mini = std::min(mini, nums[n - 2] - nums[2]); // Remove the 2 smallest and 1 largest
-        mini = std::min(mini, nums[n - 3] - nums[1]); // Remove the 1 smallest and 2 largest
-        mini = std::min(mini, nums[n - 4] - nums[0]); // Remove the 3 largest
-
+        for(int k = 0 ; k<= 3 ; k++){
+            // cout<<nums[nums.size()-1-nums[jx[k]]]<<endl;
+            mini= min(mini,(nums[nums.size()-1-jx[k]] - nums[ix[k]]));
+        }
 
         return mini;
     }
