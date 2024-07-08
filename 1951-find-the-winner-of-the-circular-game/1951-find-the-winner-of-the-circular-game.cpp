1c5f3baddef9be6325e3f1ac63fadc7541b8717d
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-07-09 03:17:58 +0530

    Time: 10 ms (40.73%), Space: 7.6 MB (41.48%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> v ; 
+        for(int i = 1 ; i <= n ; i++ ){
+            v.push_back(i);
+        }
+        int pos = k-1 ;
+
+        while(v.size()!=1){
+            // cout<<pos<<'p'<<endl;
+            // cout<<v[pos]<<endl;
+            v.erase(v.begin()+pos);
+            // pos = pos;
+            // for(auto it : v){
+            //     cout<<it<<' ';
+            // }
+            cout<<endl;
+            // if(pos==v.size()){
+                if(pos!=0)
+                    pos= pos-1;
+                else
+                    pos=v.size()-1;
+            // }
+            // else{
+
+            // }
+            if(v.size()==1)
+                break ; 
+            // if(pos+k>(v.size()))
+            pos = (pos+k)%(v.size());
+            // else
+            //     pos=pos+k;
+        }
+
+        return v[0];
+    }
+};
