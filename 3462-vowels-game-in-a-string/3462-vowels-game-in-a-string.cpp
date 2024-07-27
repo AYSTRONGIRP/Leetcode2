87f92204a6b8eda4ff209e02754d4917fe7447ba
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-07-27 22:05:46 +0530

    Time: 39 ms (89.69%), Space: 17.6 MB (30.65%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i = 0 ; i < n ; i++){
             if(s[i]=='a' || s[i]=='e' ||  s[i]=='i' ||  s[i]=='o' ||  s[i]=='u')
-                res++;
+                return true;
         }
 
         if(res>0)
